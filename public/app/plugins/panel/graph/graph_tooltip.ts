875626ff9cc530bdd0220ab231cf774ea81a4cfa
Dmitry Lukyanov <dmitry.al.lukyanov@gmail.com>
2019-03-22 18:49:40 +0300

    Legend:  Init value so that it does not brake series sorting

--- public/app/plugins/panel/graph/graph_tooltip.ts
+++ public/app/plugins/panel/graph/graph_tooltip.ts
@@ -81,6 +81,12 @@ export default function GraphTooltip(elem, dashboard, scope, getSeriesFn) {
         continue;
       }
 
+      if (series.hideFromLegend(panel.legend)) {
+        // Init value so that it does not brake series sorting
+        results[0].push({ hidden: true, value: 0 });
+        continue;
+      }
+
       hoverIndex = this.findHoverIndexFromData(pos.x, series);
       hoverDistance = pos.x - series.data[hoverIndex][0];
       pointTime = series.data[hoverIndex][0];
